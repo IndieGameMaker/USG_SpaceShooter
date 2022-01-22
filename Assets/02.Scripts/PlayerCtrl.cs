e2caf5fd8411f8f25754a318edcf1e5784001ec6
Lee Jae Hyun <myleje@gmail.com>
2022-01-22 15:35:48 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private float h, v, r;
+
     void Start()
     {
 
@@ -11,8 +13,20 @@ public class PlayerCtrl : MonoBehaviour
 
     void Update()
     {
+        v = Input.GetAxis("Vertical");
+
         //transform.position += new Vector3(0, 0, 0.1f);
         transform.Translate(Vector3.forward * 0.1f);
 
     }
+
+    /*
+        정규화벡터(Normalized Vector), 단위벡터(Unit Vector) : 크기가 1인 벡터의 단위
+        Vector3.forward = Vector3(0, 0, 1)
+        Vector3.up      = Vector3(0, 1, 0)
+        Vector3.right   = Vector3(1, 0, 0)
+
+        Vector3.one  = Vector3(1, 1, 1)
+        Vector3.zero = Vector3(0, 0, 0)
+    */
 }
