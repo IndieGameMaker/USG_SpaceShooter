9547e3878b8fb29b82b6b4b54fbd9b8dd652ed77
Lee Jae Hyun <myleje@gmail.com>
2022-02-05 11:53:05 +0900

    .

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
+
+/*
+    충돌 콜백함수 (Collision Dectect Callback Function/Event)
+
+    1. 양쪽 다 Collider 컴포넌트가 존재
+    2. 이동하는 물체에 Rigidbody 컴포넌트가 존재
+
+    IsTrigger 속성이 언체크일 경우
+    OnCollisionEnter --- 1
+    OnCollisionStay  --- n
+    OnCollisionExit  --- 1
+
+    IsTrigger 속성이 체크일 경우
+    OnTriggerEnter
+    OnTriggerStay
+    OnTriggerExit
+*/
