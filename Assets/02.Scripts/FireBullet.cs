3d1eb849c76d7452559508517f38511e447787d6
Lee Jae Hyun <myleje@gmail.com>
2022-02-05 14:38:49 +0900

    FireSfx 적용

--- Assets/02.Scripts/FireBullet.cs
+++ Assets/02.Scripts/FireBullet.cs
@@ -11,6 +11,8 @@ public class FireBullet : MonoBehaviour
 
     // 총 발사 음원(AudioClip)
     public AudioClip fireSfx;
+
+    [System.NonSerialized]
     // 오디오소스 컴포넌트
     public AudioSource audio;
 
@@ -21,12 +23,20 @@ public class FireBullet : MonoBehaviour
 
     void Update()
     {
+        Fire();
+    }
+
+    void Fire()
+    {
         // 마우스 왼쪽 버튼을 클릭 할 때 마다
         if (Input.GetMouseButtonDown(0))
         {
             // 총알을 FirePos 위치에 생성
             // Instantiate (생성할_객체, 좌표값, 회전값)
             Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+
+            // 총소리 발생
+            audio.PlayOneShot(fireSfx, 0.8f);
         }
     }
 }
